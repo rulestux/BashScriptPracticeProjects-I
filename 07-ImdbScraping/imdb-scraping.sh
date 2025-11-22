f3782aba6af09a916f1b91d18dc03a1f9f39a994
rulestux <rulestux@gmail.com>
2025-11-22 15:46:36 -0300

    IMDB Scraping

---
+++ 07-ImdbScraping/imdb-scraping.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+#################################################################
+# imdb-scraping.sh                                              #
+#                                                               #
+# Site:         https://github.com/rulestux                     #
+# Author:       Jean Felipe                                     #
+# Maintenance:  Jean Felipe                                     #
+#                                                               #
+#################################################################
+# This script returns data about top movies published in IMDB,  #
+# such as a web scraper, and creates a file with the output     #
+# in CSV format.                                                #
+#                                                               #
+# Usage example:                                                #
+#   ./imdb-scraping.sh <parameters>                             #
+#                                                               #
+#################################################################
+# History:                                                      #
+#   v1.0 2025-11-22, Jean Felipe.                               #
+#                                                               #
+#################################################################
+# Tested on:                                                    #
+#   zsh v5.9                                                    #
+#                                                               #
+#################################################################
+
+
