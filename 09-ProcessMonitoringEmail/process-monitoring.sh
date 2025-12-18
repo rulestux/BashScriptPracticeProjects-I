748a7bb3c65d1704c498893db506953336d8819b
TuxRules <rulestux@github.com>
2025-12-18 18:50:37 -0300

    Process Monitoring and send Email

---
+++ 09-ProcessMonitoringEmail/process-monitoring.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+#################################################################
+# process-monitoring.sh                                         #
+#                                                               #
+# Site:         https://github.com/rulestux                     #
+# Author:       Jean Felipe                                     #
+# Maintenance:  Jean Felipe                                     #
+#                                                               #
+#################################################################
+# This script runs as a daemon, monitoring a process entered as #
+# parameter, sending messages to an email when the process is   #
+# interrupted.                                                  #
+#                                                               #
+# Usage example:                                                #
+#   ./process-monitoring.sh <processname>                       #
+#                                                               #
+#    Exit with 'CTRL+c'.                                        #
+#                                                               #
+#################################################################
+# History:                                                      #
+#   v1.0 2025-10-12, Jean Felipe.                               #
+#   v2.0 2025-12-28, Jean Felipe.                               #
+#       send email implemented                                  #
+#                                                               #
+#################################################################
+# Tested on:                                                    #
+#   zsh v5.9                                                    #
+#                                                               #
+#################################################################
+
+# variável para definir o intervalo entre as execuções:
+TIME=5
+EMAIL=rulestux@gmail.com
+
+# teste para verificar se algum argumento foi informado em
+# qualquer posição '#'; se o valor for zero, mensagem de erro:
+if [[ $# -eq 0 ]]
+then
+    echo "Please, insert a process as argument:"
+    echo "\"./process-monitoring.sh <processname>\""
+    exit 1
+fi
+
+# início de um loop infinito 'enquanto true':
+while true
+do
+    # se 'grep' encontrar o processo contido em '$1', excetuando
+    # outros elementos do comando (o próprio grep e a posição
+    # '$0' da chamada do comando), apenas aguardar 'sleep $TIME'
+    # e voltar a verificar:
+    if ps aux | grep $1 | grep -v grep | grep -v $0 > /dev/null
+    then
+        sleep $TIME
+    else
+        # do contrário, enviar a mensagem de erro para o
+        # email com o comando 'mail', aguardar 'sleep $TIME' e
+        # tornar a verificar:
+        echo "WARNING! Process $1 is not running!" | mail -s "[$0] monitoring $1" $EMAIL
+        sleep $TIME
+    fi
+done
+
